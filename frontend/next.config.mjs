8b77eddab61d88d3b6be489c2890bc061180616c
Thanin Kongkiatsophon <108406347+thaninbew@users.noreply.github.com>
2026-09-19 14:38:08 -0400

    feat: serve Campco prototype from frontend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/campco.html",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
